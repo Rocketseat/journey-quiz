3b80dbdb3896711e0d8b58edb42a7d04b8290580
Diego Fernandes <diego.schell.f@gmail.com>
2022-09-23 17:02:46 -0300

    fix: Do not check url type on db url env

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -3,7 +3,7 @@
 import { z } from 'zod'
 
 export const serverSchema = z.object({
-  DATABASE_URL: z.string().url(),
+  DATABASE_URL: z.string(),
   NODE_ENV: z.enum(['development', 'test', 'production']),
 })
 
